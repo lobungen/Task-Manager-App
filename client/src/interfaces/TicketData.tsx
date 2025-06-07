4c17d848ec6b7674fd1b98ed0e2635e9ad50d370
Lee Obungen <lobungen@github.com>
2025-06-06 21:25:19 -0500

    updated interfaces

--- client/src/interfaces/TicketData.tsx
+++ client/src/interfaces/TicketData.tsx
@@ -1,10 +1,10 @@
 import { UserData } from './UserData';
 
 export interface TicketData {
-  id: number; 
+  id: string;
   name: string;
   description: string;
   status: string;
-  assignedUserId: number;
-  assignedUser: UserData | null; // Optional, can be null if not assigned
+  assignedUserId: string;
+  assignedUser: UserData | null;
 }
